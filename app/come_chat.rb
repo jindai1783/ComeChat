1f934c2589e64971531f5c6c74cfd22f38c3ebcc
Jin Dai <jindai1783@gmail.com>
2015-01-19 17:57:06 +0000

    + Duplication registration prevention function

--- app/come_chat.rb
+++ app/come_chat.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'data_mapper'
+require 'rack-flash'
 
 require './app/models/message'
 require './app/models/user'
@@ -19,6 +20,7 @@ class ComeChat < Sinatra::Base
   include Helpers
 
   enable :sessions
+  use Rack::Flash
   set :session_secret, 'super secret'
 
   get '/' do
@@ -35,19 +37,29 @@ class ComeChat < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :"users/new"
   end
 
   post '/users' do
     puts "\e[34mREADY TO CREATE NEW USER\e[0m"
     puts "password confirmation = " + params['password_confirmation'].to_s
-    user = User.create(:email => params[:email],
+    @user = User.create(:email => params[:email],
                 :password => params[:password],
                 :password_confirmation => params[:password_confirmation])
-    puts "\e[34mNEW USER CREATED\e[0m"
-    puts user.inspect
-    session[:user_id] = user.id
-    redirect to('/')
+
+    if @user.save
+      puts "\e[34mNEW USER CREATED\e[0m"
+      puts @user.inspect
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      puts "\e[33mUSER NOT CREATED\e[0m"
+      puts @user.inspect
+      flash.now[:errors] = @user.errors.full_messages
+      puts "\e[33mTEST POINT\e[0m"
+      erb :"users/new"
+    end
   end
 
   # start the server if ruby file executed directly
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true, :message => "This email is already registered"
   property :password_digest, Text
 
   attr_reader :password
   attr_accessor :password_confirmation
 
-  validates_confirmation_of :password
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
 
   def password=(password)
     puts "\e[34mGENERATING PASSWORD DIGEST\e[0m"
